605d3aa45ebaa5a70377768dec4c64bb7b4da9d7
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-09-19 02:05:55 -0400

    Add documentation to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import "./styles.scss";
 
+// using a list of maps with `value` and `path` keys lets us
+// very easily change the path without affecting any other logic
+// in the program
 const navElements = [
     {
         value: "Home",
@@ -21,6 +24,8 @@ class Header extends Component {
     state = {};
 
     render() {
+        // `this.props.active` is the value rather than the slug
+        // e.g. 'Generate Report' instead of 'generate-report'
         let { active } = this.props;
         return (
             <header>
@@ -37,6 +42,12 @@ class Header extends Component {
                         })}
                     </div>
                 </nav>
+                {/*
+                    the `spacer` class exists to push page content down 
+                    because the nav has `position: fixed`, so without the
+                    `spacer` the nav would sit on top of the page content
+                */}
+                <div className="spacer"></div>
             </header>
         );
     }
