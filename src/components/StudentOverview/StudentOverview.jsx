df7dccba5928ce0ae9c868c02b377da6350b81f8
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-09-19 23:48:34 -0400

    Add basic functionality to StudentOverview

--- src/components/StudentOverview/StudentOverview.jsx
+++ src/components/StudentOverview/StudentOverview.jsx
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
+import UpdateStudentForm from 'components/UpdateStudentForm';
 import "./styles.scss";
 
 class StudentOverview extends Component {
-    state = {};
+    state = {
+        // whether or not to show edit info form
+        isEditing: false
+    };
+
+    toggleIsEditing = () => {
+        const { isEditing } = this.state;
+        this.setState({ isEditing: !isEditing });
+    }
 
     render() {
-        return ();
+        const { first, last, hours } = this.props;
+        const { isEditing } = this.state;
+        return (
+            <div className="overview-wrapper">
+                <div className="overview-text-wrapper">
+                    <div className="name-wrapper">
+                        <span className="name">
+                            Alex Jones
+                        </span>
+                        <div>
+                            <span className="hours">
+                                25
+                            </span>
+                            <span className="hours-label">
+                                hours
+                            </span>
+                        </div>
+                    </div>
+                    <div className="bar-wrapper">
+                        <div className="bar" style={{ width: `${Math.min(Math.ceil((25 / 200) * 100), 100)}%` }} />
+                    </div>
+                    <div className="icon-bar-wrapper">
+                        <div className="edit-icon" onClick={ this.toggleIsEditing } />
+                        <div className="delete-icon" />
+                    </div>
+                </div>
+                <UpdateStudentForm style={{ marginTop: "20px", display: isEditing ? "block" : "none" }}/>
+            </div>
+        );
     }
 }
 
