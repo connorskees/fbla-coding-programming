401e871a0779d3830d9eb38a929a6e32f82277b6
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-09-22 14:19:33 -0400

    Add update functionality to student form

--- src/components/UpdateStudentForm/UpdateStudentForm.jsx
+++ src/components/UpdateStudentForm/UpdateStudentForm.jsx
@@ -5,60 +5,92 @@ import "./styles.scss";
 const illegalCharacters = "[^<>(){}'\"]+"
 
 class UpdateStudentForm extends Component {
-    state = {};
+    state = {
+        ...this.props.student,
+    };
+
+    updateFirst = (event) => {
+        this.setState({ first: event.target.value })
+    }
+
+    updateLast = (event) => {
+        this.setState({ last: event.target.value })
+    }
+
+    updateHours = (event) => {
+        this.setState({ volunteer_hours: event.target.value })
+    }
+
+    updateGrade = (event) => {
+        this.setState({ grade: event.target.value })
+    }
+
+    updateStudentID = (event) => {
+        this.setState({ student_id: event.target.value })
+    }
+
+    updateCommunityServiceAward = (event) => {
+        this.setState({ community_service_award: event.target.value })
+    }
+
+    save = () => {
+
+    }
+    
 
     render() {
         // assumes style is a valid "CSS in JS" style object
         // studentUUID is primary key of student, to propagate data from db
-        const { style, studentUUID } = this.props;
+        const { style } = this.props;
+        const { first, last, volunteer_hours, grade, student_id, community_service_award } = this.state;
         return (
             <form className="update-student-form" style={style}>
                 <div className="row row-1">
                     <label htmlFor="first">
                         First
-                        <input required pattern={ illegalCharacters } type="text" id="first" />
+                        <input required onChange={this.updateFirst} value={first} pattern={ illegalCharacters } type="text" id="first" />
                     </label>
 
                     <label htmlFor="last">
                         Last
-                        <input required pattern={ illegalCharacters } type="text" id="last" />
+                        <input required onChange={this.updateLast} value={last} pattern={ illegalCharacters } type="text" id="last" />
                     </label>
                 </div>
 
                 <div className="row row-2">
                     <label htmlFor="hours">
                         Volunteer Hours
-                        <input required pattern={ illegalCharacters } type="number" id="hours" />
+                        <input required onChange={this.updateHours} value={volunteer_hours} pattern={ illegalCharacters } type="number" id="hours" />
                     </label>
 
                     <label htmlFor="grade">
                         Grade
-                        <select required id="grade">
-                            <option>9</option>
-                            <option>10</option>
-                            <option>11</option>
-                            <option>12</option>
+                        <select required onChange={this.updateGrade} value={grade} id="grade">
+                            <option value={9}>9</option>
+                            <option value={10}>10</option>
+                            <option value={11}>11</option>
+                            <option value={12}>12</option>
                         </select>
                     </label>
 
                     <label htmlFor="student-id">
                         Student ID
-                        <input required pattern={ illegalCharacters } type="text" id="student-id" />
+                        <input required onChange={this.updateStudentID} value={student_id} pattern={ illegalCharacters } type="text" id="student-id" />
                     </label>
                 </div>
 
                 <div className="row row-3">
                     <label htmlFor="award">
                         Community Service Award
-                        <select id="award">
-                            <option>Community</option>
-                            <option>Service</option>
-                            <option>Achievement</option>
+                        <select onChange={this.updateCommunityServiceAward} value={community_service_award} id="award">
+                            <option value={"Community"}>Community</option>
+                            <option value={"Service"}>Service</option>
+                            <option value={"Achievement"}>Achievement</option>
                         </select>
                     </label>
                 </div>
 
-                <button type="submit" className="update">Update</button>
+                <button type="button" onClick={(x) => this.props.onSave(this.state)} className="update">Update</button>
             </form>
         );
     }
