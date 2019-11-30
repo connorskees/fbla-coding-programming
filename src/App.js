1c20bcf0cd5c44bfe20441e20d167eda1280d9c2
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-11-29 21:36:45 -0500

    Add menu

--- src/App.js
+++ src/App.js
@@ -7,6 +7,41 @@ import {
   Route,
 } from "react-router-dom";
 import './App.css';
+import db from "db";
+
+const { app, Menu } = window.require("electron").remote;
+
+const menu = Menu.buildFromTemplate([
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Exit",
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  },
+  {
+    label: "Help",
+    submenu: [
+      {
+        label: "Students",
+        click() {
+          window.location = "file:///index.html#/";
+        }
+      },
+      {
+        label: "Generate Report",
+        click() {
+          window.location = "file:///index.html#/";
+        }
+      }
+    ]
+  }
+]);
+Menu.setApplicationMenu(menu);
 
 class App extends Component {
   componentWillUnmount() {
